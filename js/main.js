d200657ec58fe5cb52d1acbcebeb88cfaef74108
Sara Roohan <sara.roohan@gmail.com>
2021-05-17 15:24:10 -0700

    Listen for 'submit' events on the journal entry form. #1

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
 /* global data */
 /* exported data */
 
-function handleInput(event) {
-  $img.src = event.target.value;
-}
-
-const $photoUrl = document.querySelector('.photo-url');
 const $img = document.querySelector('img');
-$photoUrl.addEventListener('input', handleInput);
+const $photoUrl = document.querySelector('.url');
+
+$photoUrl.addEventListener('input', function (event) {
+  $img.setAttribute('src', event.target.value);
+});
+
+const $entryForm = document.querySelector('.form');
+let nextEntryID = 1;
+
+$entryForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  nextEntryID++;
+  const formEntries = {
+    title: $entryForm.elements.title.value,
+    url: $entryForm.elements.url.value,
+    notes: $entryForm.elements.notes.value,
+    nextEntryID: nextEntryID
+  };
+  data.entries.unshift(formEntries);
+  $img.setAttribute('src', 'images/placeholder-image-square.jpg');
+  document.querySelector('.form').reset();
+});
